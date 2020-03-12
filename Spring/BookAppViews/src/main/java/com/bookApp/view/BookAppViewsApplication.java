edd6478b49eaa4f2ce7c49f478d83350af6466f0
harsh3105 <harshgrover3105@gmail.com>
2020-03-12 22:57:34 +0530

    added new view microservice

---
+++ Spring/BookAppViews/src/main/java/com/bookApp/view/BookAppViewsApplication.java
@@ -0,0 +1,13 @@
+package com.bookApp.view;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookAppViewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookAppViewsApplication.class, args);
+	}
+
+}
