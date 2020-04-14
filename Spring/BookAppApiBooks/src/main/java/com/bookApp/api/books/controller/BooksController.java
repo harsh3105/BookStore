1b04e4fda43f41b73fa2a99ff9af926c83faca02
harsh3105 <harshgrover3105@gmail.com>
2020-04-14 22:20:58 +0530

    chnaged book-ws

--- Spring/BookAppApiBooks/src/main/java/com/bookApp/api/books/controller/BooksController.java
+++ Spring/BookAppApiBooks/src/main/java/com/bookApp/api/books/controller/BooksController.java
@@ -1,18 +1,14 @@
 package com.bookApp.api.books.controller;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,9 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bookApp.api.books.model.Book;
-import com.bookApp.api.books.model.BooksQuantity;
 import com.bookApp.api.books.repository.BookRepository;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 @RequestMapping("/books")
