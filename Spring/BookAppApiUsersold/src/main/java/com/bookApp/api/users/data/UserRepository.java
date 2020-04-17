46f638bedfb2e74ee91ab6885af6dd45c4c2db73
harsh3105 <harshgrover3105@gmail.com>
2020-04-17 20:36:38 +0530

    Deleted all unwanted files

--- Spring/BookAppApiUsersold/src/main/java/com/bookApp/api/users/data/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bookApp.api.users.data;
-
-import org.springframework.data.cassandra.repository.CassandraRepository;
-
-public interface UserRepository extends CassandraRepository<UserEntity, Long> {
-
-}
--- Spring/BookAppApiUsersold/src/main/java/com/bookApp/api/users/service/UsersService.java
+++
@@ -1,9 +0,0 @@
-package com.bookApp.api.users.service;
-
-import com.bookApp.api.users.shared.UserDto;
-
-public interface UsersService {
-	
-	UserDto createUser(UserDto Userdetails);
-
-}
